732d7c2b08d3bc9d12b0b8663cc6da01366e377e
Kim Johnson <memjay3279@gmail.com>
2021-10-12 11:10:30 -0400

    Add app controller logic and sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+    def token(user_id)
+        payload = { user_id: user_id }
+        JWT.encode(payload, ENV["HMAC_SECRET"], 'HS256')
+      end
+
+    def client_has_valid_token?
+        !!current_user_id
+    end
+    
+    def current_user_id
+        begin
+          token = request.headers["Authorization"]
+          decoded_array = JWT.decode(token, ENV["HMAC_SECRET"], true, { algorithm: 'HS256' })
+          payload = decoded_array.first
+        rescue #JWT::VerificationError
+          return nil
+        end
+        payload["user_id"]
+      end
+    
+      def require_login
+        render json: {error: 'Unauthorized'}, status: :unauthorized unless client_has_valid_token?
+      end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,9 @@
 class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
-
         if user
-            session[:user_id] = user.id
-            payload = { user_id: user.id }
             render json: {
-                token: JWT.encode(payload, nil, 'HS256')
+                token: token(user.id)
             }
         else
             render json: {
